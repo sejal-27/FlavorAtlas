f7c4dd5a2c42071081de0d1f412d5ee3d788b926
sejal-27 <sejall2709@gmail.com>
2023-08-18 10:04:23 +0530

    modal update

--- home.js
+++ home.js
@@ -1,4 +1,3 @@
-
 //JS hero section
 let text = document.getElementById("text");
 
@@ -8,6 +7,64 @@ window.addEventListener("scroll", function () {
   text.style.top = 40 + value * -0.85 + "%";
 });
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Fetch data and create carousel cards for Vegetarian meals
+//   fetchAndCreateCards("Vegetarian", "card-carousel-1");
+
+//   // Fetch data and create carousel cards for Non-Vegetarian meals
+//   fetchAndCreateCards("Chicken", "card-carousel-2");
+
+//   // Fetch data and create carousel cards for Dessert meals
+//   fetchAndCreateCards("Dessert", "card-carousel-3");
+// });
+
+// function fetchAndCreateCards(category, carouselId) {
+//   fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const carouselInner = document.querySelector(
+//         `#${carouselId} .carousel-inner`
+//       );
+//       carouselInner.innerHTML = "";
+
+//       data.meals.forEach((meal, index) => {
+//         const carouselItemClass =
+//           index === 0 ? "carousel-item active" : "carousel-item";
+//         const cardHtml = `
+//           <div class="${carouselItemClass}">
+//             <div class="card">
+//               <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}" />
+//               <div class="card-body">
+//                 <h5 class="card-title">${meal.strMeal}</h5>
+//                 <button class="btn btn-primary btn-sm view-details" data-mealid="${meal.idMeal}" data-bs-toggle="modal" data-bs-target="#mealModal">Get Recipe</button>
+//               </div>
+//             </div>
+//           </div>
+//         `;
+//         carouselInner.innerHTML += cardHtml;
+//       });
+
+//       const viewDetailsButtons = document.querySelectorAll(
+//         `#${carouselId} .view-details`
+//       );
+//       viewDetailsButtons[0].addEventListener("click", (event) => {
+//           const mealId = event.target.getAttribute("data-mealid");
+//           openMealDetailsPage(mealId);
+//         });
+//     })
+//     .catch((error) => {
+//       console.error(`Error fetching ${category} API data:`, error);
+//     });
+// }
+
+// function openMealDetailsPage(mealId) {
+
+//   window.location.href = `meal-details.html?mealId=${mealId}`;
+// }
+
+// carousel
+// Fetch the API data for Vegetarian meals
+// Fetch data and create carousel cards
 document.addEventListener("DOMContentLoaded", function () {
   // Fetch data and create carousel cards for Vegetarian meals
   fetchAndCreateCards("Vegetarian", "card-carousel-1");
@@ -48,287 +105,65 @@ function fetchAndCreateCards(category, carouselId) {
       const viewDetailsButtons = document.querySelectorAll(
         `#${carouselId} .view-details`
       );
-      viewDetailsButtons[0].addEventListener("click", (event) => {
+      viewDetailsButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
           const mealId = event.target.getAttribute("data-mealid");
-          openMealDetailsPage(mealId);
+          openMealModal(mealId);
         });
+      });
     })
     .catch((error) => {
       console.error(`Error fetching ${category} API data:`, error);
     });
 }
 
-function openMealDetailsPage(mealId) {
- 
-  window.location.href = `meal-details.html?mealId=${mealId}`;
+function openMealModal(mealId) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      const modalHtml = `
+        <div class="modal fade" id="mealModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">${meal.strMeal}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <img src="${meal.strMealThumb}" class="img-fluid" alt="${meal.strMeal}" />
+                <div class="data">
+                  <p><strong>Category:</strong> ${meal.strCategory}</p>
+                  <p><strong>Area:</strong> ${meal.strArea}</p>
+                  <p><strong>Instructions:</strong> ${meal.strInstructions}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+
+      const modalContainer = document.getElementById("modal-container");
+      modalContainer.innerHTML = "";
+      modalContainer.innerHTML = modalHtml;
+
+      const modal = new bootstrap.Modal(document.getElementById("mealModal"));
+      modal.show();
+
+      modal._element.addEventListener("hidden.bs.modal", function () {
+        document.body.style.overflow = "auto";
+        document.body.style.padding = 0; // Reset body overflow to default
+        const backdrop = document.querySelector(".modal-backdrop");
+        if (backdrop) {
+          backdrop.remove();
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching meal details:", error);
+    });
 }
 
-
-//carousel
-// Fetch the API data for Vegetarian meals
-// // Fetch data and create carousel cards
-// fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const carouselInner = document.querySelector(
-//       "#card-carousel-1 .carousel-inner"
-//     );
-
-//     // Clear any existing content
-//     carouselInner.innerHTML = "";
-
-//     // Loop through the meals and create carousel cards
-//     data.meals.forEach((meal, index) => {
-//       const carouselItemClass =
-//         index === 0 ? "carousel-item active" : "carousel-item";
-//       const cardHtml = `
-//         <div class="${carouselItemClass}">
-//           <div class="card">
-//             <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}" />
-//             <div class="card-body">
-//               <h5 class="card-title">${meal.strMeal}</h5>
-//               <button class="btn btn-primary btn-sm view-details" data-mealid="${meal.idMeal}" data-bs-toggle="modal" data-bs-target="#mealModal">Get Recipe</button>
-//             </div>
-//           </div>
-//         </div>
-//       `;
-//       carouselInner.innerHTML += cardHtml;
-//     });
-
-//     // Attach click event listener to the "View Details" buttons
-//     const viewDetailsButtons = document.querySelectorAll(".view-details");
-//     viewDetailsButtons.forEach((button) => {
-//       button.addEventListener("click", (event) => {
-//         const mealId = event.target.getAttribute("data-mealid");
-//         // Call a function to show the modal with meal details using the mealId
-//         openMealModal(mealId);
-//       });
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("Error fetching API data:", error);
-//   });
-
-
-// // Function to show modal with meal details
-// function openMealModal(mealId) {
-//   // Fetch meal details using the mealId
-//   fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const meal = data.meals[0];
-//       // Create modal content using the fetched data
-//       const modalHtml = `
-//         <div class="modal" id="mealModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-//           <div class="modal-dialog">
-//             <div class="modal-content">
-//               <div class="modal-header">
-//                 <h5 class="modal-title" id="exampleModalLabel">${meal.strMeal}</h5>
-//                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//               </div>
-//               <div class="modal-body">
-//                 <img src="${meal.strMealThumb}" class="img-fluid" alt="${meal.strMeal}" />
-//                 <div class="data">
-//                   <p><strong>Category:</strong> ${meal.strCategory}</p>
-//                   <p><strong>Area:</strong> ${meal.strArea}</p>
-//                   <p><strong>Instructions:</strong> ${meal.strInstructions}</p>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       `;
-//       // Add the modal content to the modal container
-//       const modalContainer = document.getElementById("modal-container");
-//       modalContainer.innerHTML = modalHtml;
-
-//       // Show the modal
-//       const modal = new bootstrap.Modal(document.getElementById("mealModal"));
-//       modal.show();
-
-//       // Handle modal hide event to remove the backdrop
-//       modal._element.addEventListener("hidden.bs.modal", function () {
-//         const backdrop = document.querySelector(".modal-backdrop");
-//         if (backdrop) {
-//           backdrop.remove();
-//         }
-//       });
-//     })
-//     .catch((error) => {
-//       console.error("Error fetching meal details:", error);
-//     });
-// }
-
-// // Fetch the API data for Non- Vegetarian meals
-// // Fetch data and create carousel cards
-// fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const carouselInner = document.querySelector(
-//       "#card-carousel-2 .carousel-inner"
-//     );
-
-//     // Clear any existing content
-//     carouselInner.innerHTML = "";
-
-//     // Loop through the meals and create carousel cards
-//     data.meals.forEach((meal, index) => {
-//       const carouselItemClass =
-//         index === 0 ? "carousel-item active" : "carousel-item";
-//       const cardHtml = `
-//         <div class="${carouselItemClass}">
-//           <div class="card">
-//             <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}" />
-//             <div class="card-body">
-//               <h5 class="card-title">${meal.strMeal}</h5>
-             
-//               <button class="btn btn-primary btn-sm view-details" data-mealid="${meal.idMeal}" data-bs-toggle="modal" data-bs-target="#mealModal">Get Recipe</button>
-//             </div>
-//           </div>
-//         </div>
-//       `;
-//       carouselInner.innerHTML += cardHtml;
-//     });
-
-//     // Attach click event listener to the "View Details" buttons
-//     const viewDetailsButtons = document.querySelectorAll(".view-details");
-//     viewDetailsButtons.forEach((button) => {
-//       button.addEventListener("click", (event) => {
-//         const mealId = event.target.getAttribute("data-mealid");
-//         // Call a function to show the modal with meal details using the mealId
-//         openMealModal(mealId);
-//       });
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("Error fetching API data:", error);
-//   });
-
-// // Function to show modal with meal details
-// function openMealModal(mealId) {
-//   // Fetch meal details using the mealId
-//   fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const meal = data.meals[0];
-//       // Create modal content using the fetched data
-//       const modalHtml = `
-//         <div class="modal fade" id="mealModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-//           <div class="modal-dialog modal-fullscreen">
-//             <div class="modal-content">
-//               <div class="modal-header">
-//                 <h5 class="modal-title" id="exampleModalLabel">${meal.strMeal}</h5>
-//                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//               </div>
-//               <div class="modal-body">
-              
-//                 <img src="${meal.strMealThumb}" class="img-fluid" alt="${meal.strMeal}" />
-//                 <div class="data"><p><strong>Category:</strong> ${meal.strCategory}</p>
-//                 <p><strong>Area:</strong> ${meal.strArea}</p>
-//                 <p><strong>Instructions:</strong> ${meal.strInstructions}</p>
-//               </div></div>
-             
-                
-      
-//             </div>
-//           </div>
-//         </div>
-//       `;
-//       // Add the modal content to the modal container
-//       const modalContainer = document.getElementById("modal-container");
-//       modalContainer.innerHTML = modalHtml;
-
-//       // Show the modal
-//       const modal = new bootstrap.Modal(document.getElementById("mealModal"));
-//       modal.show();
-//     })
-//     .catch((error) => {
-//       console.error("Error fetching meal details:", error);
-//     });
-// }
-
-// // Fetching the API data for Desserts
-// fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const carouselInner = document.querySelector(
-//       "#card-carousel-3 .carousel-inner"
-//     );
-
-//     // Clear any existing content
-//     carouselInner.innerHTML = "";
-
-//     // Loop through the meals and create carousel cards
-//     data.meals.forEach((meal, index) => {
-//       const carouselItemClass =
-//         index === 0 ? "carousel-item active" : "carousel-item";
-//       const cardHtml = `
-//         <div class="${carouselItemClass}">
-//           <div class="card">
-//             <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}" />
-//             <div class="card-body">
-//               <h5 class="card-title">${meal.strMeal}</h5>
-              
-//               <button class="btn btn-primary btn-sm view-details" data-mealid="${meal.idMeal}" data-bs-toggle="modal" data-bs-target="#mealModal">Get Recipe</button>
-//             </div>
-//           </div>
-//         </div>
-//       `;
-//       carouselInner.innerHTML += cardHtml;
-//     });
-
-//     // Attach click event listener to the "View Details" buttons
-//     const viewDetailsButtons = document.querySelectorAll(".view-details");
-//     viewDetailsButtons.forEach((button) => {
-//       button.addEventListener("click", (event) => {
-//         const mealId = event.target.getAttribute("data-mealid");
-//         // Call a function to show the modal with meal details using the mealId
-//         openMealModal(mealId);
-//       });
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("Error fetching API data:", error);
-//   });
-
-// // Function to show modal with meal details
-// function openMealModal(mealId) {
-//   // Fetch meal details using the mealId
-//   fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const meal = data.meals[0];
-//       // Create modal content using the fetched data
-//       const modalHtml = `
-//         <div class="modal fade" id="mealModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-//           <div class="modal-dialog modal-fullscreen">
-//             <div class="modal-content">
-//               <div class="modal-header">
-//                 <h5 class="modal-title" id="exampleModalLabel">${meal.strMeal}</h5>
-//                  <button type="button" class="btn btn-flip"  data-bs-dismiss="modal" data-back="Close" data-front="Close"></button>
-//               </div>
-//               <div class="modal-body">
-//                 <img src="${meal.strMealThumb}" class="img-fluid" alt="${meal.strMeal}" />
-//                 <div class="data"><p><strong>Category:</strong> ${meal.strCategory}</p>
-//                 <p><strong>Area:</strong> ${meal.strArea}</p>
-//                 <p><strong>Instructions:</strong> ${meal.strInstructions}</p>
-             
-//             </div></div></div>
-//           </div>
-//         </div>
-//       `;
-//       // Add the modal content to the modal container
-//       const modalContainer = document.getElementById("modal-container");
-//       modalContainer.innerHTML = modalHtml;
-
-//       // Show the modal
-//       const modal = new bootstrap.Modal(document.getElementById("mealModal"));
-//       modal.show();
-//     })
-//     .catch((error) => {
-//       console.error("Error fetching meal details:", error);
-//     });
-// }
 //loader
 ("use strict");
 
